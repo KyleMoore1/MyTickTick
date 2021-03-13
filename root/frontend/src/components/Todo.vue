a13de27c064ff0026388dabe85e9ffccda7f345a
Kyle Moore <kmoore1@scu.edu>
2021-03-12 21:57:27 -0800

    added todolist

---
+++ root/frontend/src/components/Todo.vue
@@ -0,0 +1,30 @@
+<template>
+<div id = "todo">
+  <input type="checkbox" id="checkbox">
+  <h3>{{todo.title}}</h3>
+  <p>{{todo.project}}</p>
+  <p>10:00am</p>
+</div>
+</template>
+
+<script>
+export default {
+  name: "Todo",
+  props: ["todo"]
+};
+</script>
+
+<style scoped>
+
+#todo {
+  white-space: nowrap;
+}
+
+#todo * {
+  display: inline;
+}
+
+#checkbox {
+  padding-right: 20px;
+}
+</style>
---
+++ root/frontend/src/components/TodoList.vue
@@ -0,0 +1,57 @@
+<template>
+  <div>
+    <h1>Todos</h1>
+    <ul id = "todos">
+      <li v-for="todo in todos" :key="todo.title">
+        <Todo v-bind:todo="todo"/>
+      </li>
+    </ul>
+  </div>
+
+</template>
+
+<script>
+import Todo from "@/components/Todo";
+export default {
+  name: "TodoList",
+  components: {Todo},
+  data() {
+    return {
+      todos: [
+        {
+          title: "title",
+          due_date: "2020-03-20",
+          priority: 0,
+          description: "description",
+          project: "project",
+          isComplete: false
+        },
+        {
+          title: "title",
+          due_date: "2020-03-20",
+          priority: 0,
+          project: "project",
+          description: "description",
+          isComplete: false
+        },
+        {
+          title: "title",
+          due_date: "2020-03-20",
+          priority: 0,
+          project: "project",
+          description: "description",
+          isComplete: false
+        }
+      ]
+    }
+  }
+};
+</script>
+
+<style scoped>
+
+ul {
+  list-style-type: none;
+}
+
+</style>
