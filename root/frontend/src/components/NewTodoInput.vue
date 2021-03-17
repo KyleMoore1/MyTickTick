17244f19f2e0aafd6cc4f53b15dbaa43f9035412
Kyle Moore <kmoore1@scu.edu>
2021-03-16 20:10:25 -0700

    basic styling for input done

--- root/frontend/src/components/NewTodoInput.vue
+++ root/frontend/src/components/NewTodoInput.vue
@@ -1,5 +1,9 @@
 <template>
- <h1>New Todo Input</h1>
+  <div id = "container">
+    <div id = "todo-input">
+      <input type="text" placeholder="Add Task to Inbox. Press Enter to save.">
+    </div>
+  </div>
 </template>
 
 <script>
@@ -10,4 +14,20 @@ name: "NewTodoInput"
 
 <style scoped>
 
+#container {
+  margin: 12px 28px 20px;
+}
+
+#todo-input {
+  background-color: rgba(44, 66, 118, 0.06);
+  margin: auto;
+  height: 40px;
+}
+
+input {
+  width: 100%;
+  height: 40px;
+  padding-left: 12px;
+  padding-right: 12px;
+}
 </style>
