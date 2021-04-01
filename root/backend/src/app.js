947992e832e148fbe610f21d5791ce64bffde1a7
Kyle Moore <kmoore1@scu.edu>
2021-04-01 15:03:30 -0700

    init todo controller

--- root/backend/src/app.js
+++ root/backend/src/app.js
@@ -1,11 +1,11 @@
 const express = require("express");
+const path = require("path");
 
 const app = express();
 
 const PORT = process.env.PORT || 5000;
 
-app.get("/", (req, res) => {
-  res.send("<h1>Hello World!!</h1>");
-});
+const todo_controller = require("./controller/todos");
+app.use("/todos", todo_controller);
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
