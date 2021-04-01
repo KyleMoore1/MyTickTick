91080afe0b0b9809728f30c59fd918e6cecd1066
Kyle Moore <kmoore1@scu.edu>
2021-04-01 16:53:46 -0700

    able to post and get todos

--- root/backend/src/controller/todos.js
+++ root/backend/src/controller/todos.js
@@ -1,8 +1,40 @@
 const express = require("express");
 const router = express.Router();
+const Todo = require("../models/todo");
 
-router.get("/", (req, res) => {
-  res.send("Cool");
+//getting all
+router.get("/", async (req, res) => {
+  try {
+    const allTodos = await Todo.find();
+    res.json(allTodos);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
 });
+
+//adding one
+router.post("/", async (req, res) => {
+  const body = req.body;
+  const todo = new Todo({
+    title: body.title,
+    due_date: body.due_date,
+    priority: body.priority,
+    description: body.description,
+    project: body.project,
+    isComplete: body.isComplete
+  });
+
+  try {
+    const newTodo = await todo.save();
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+});
+
+//updating one
+router.put("/:id", (req, res) => {});
+
+//deleting one
+router.delete("/:id", (req, res) => {});
 
 module.exports = router;
